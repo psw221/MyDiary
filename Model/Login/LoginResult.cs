3ee78bd86941b4cac4433c7cb7141c32e00ab600
psw221 <tpdnddlek@gmail.com>
2018-06-13 01:13:24 +0900

    etc : Model class 생성

---
+++ Model/Login/LoginResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDiary.Model.Login
+{
+    public class LoginResult
+    {
+        public string success { get; set; }
+        public string message { get; set; }
+        public string errors { get; set; }
+        public string data { get; set; }
+    }
+}
---
+++ Model/Login/SignUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDiary.Model.Login
+{
+    public class SignUp
+    {
+        public string username { get; set; }
+        public string user { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string passwordConfirmation { get; set; }
+    }
+}
