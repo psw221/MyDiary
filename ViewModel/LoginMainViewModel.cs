6426d1e0da308833cca1dfcd134ea178da5f117d
psw221 <tpdnddlek@gmail.com>
2018-06-13 22:54:49 +0900

    add : 로그인 view, viewmodel 추가

--- ViewModel/LoginMainViewModel.cs
+++ ViewModel/LoginMainViewModel.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyDiary.ViewModel.Login
+namespace MyDiary.ViewModel
 {
-    class LoginMainWindowViewModel
+    public class LoginMainViewModel
     {
     }
 }
--- Views/LoginMainView.xaml.cs
+++ Views/LoginMainView.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MyDiary.Views.Login
+namespace MyDiary.Views
 {
     /// <summary>
-    /// LoginMainWindow.xaml에 대한 상호 작용 논리
+    /// LoginMainView.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class LoginMainWindow : Window
+    public partial class LoginMainView : UserControl
     {
-        public LoginMainWindow()
+        public LoginMainView()
         {
             InitializeComponent();
         }
