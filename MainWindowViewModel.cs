0537b7d8d571ea3dc66e5f0d540a54a2d058fadc
psw221 <tpdnddlek@gmail.com>
2018-06-13 23:29:30 +0900

    etc : MainWindow - MainWindowViewModel 연결

--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyDiary.Classes;
+using MyDiary.Views;
 
 namespace MyDiary
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : BindableBase
     {
+        public MainWindowViewModel()
+        {
+            CurrentView = loginMainView;
+        }
+
+        private LoginMainView loginMainView = new LoginMainView();
+
+        private object _CurrentView;
+
+        public object CurrentView
+        {
+            get { return _CurrentView; }
+            set { SetProperty(ref _CurrentView, value); }
+        }
     }
 }
