84ed04a7b98995145a5f4a4aeff1b679cf6bd434
psw221 <tpdnddlek@gmail.com>
2018-06-16 00:53:47 +0900

    style : Main UI 개발 및 Login UI 수정

--- Views/DiaryMainView.xaml.cs
+++ Views/DiaryMainView.xaml.cs
@@ -24,5 +24,17 @@ namespace MyDiary.Views
         {
             InitializeComponent();
         }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+        }
     }
 }
