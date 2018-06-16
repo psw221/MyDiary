b8ada611132e84858a9ae0ab758a58037dfc5874
psw221 <tpdnddlek@gmail.com>
2018-06-17 02:43:27 +0900

    refactor : DiaryMainView - DiaryMainViewModel 연결 작업 feat : DiaryMain 항목 선택별 ViewChage기능 추가

---
+++ ViewModel/DiaryMainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyDiary.Classes;
+using MyDiary.Views;
+
+namespace MyDiary.ViewModel
+{
+    public class DiaryMainViewModel : BindableBase
+    {
+        public DiaryMainViewModel()
+        {
+            _CurrentDiaryView = diaryMainView;
+
+            DiaryCommand = new MyICommand<string>(DiaryViewChange);
+        }
+
+        private DiaryBoardContentView diaryBoardView = new DiaryBoardContentView();
+        private DiaryCalendarContentView diaryCalendarView = new DiaryCalendarContentView();
+        private DiaryWirteContentView diaryWriteView = new DiaryWirteContentView();
+        private DiaryMainContentView diaryMainView = new DiaryMainContentView();
+
+        private object _CurrentDiaryView;
+
+        public object CurrentDiaryView
+        {
+            get { return _CurrentDiaryView; }
+            set { SetProperty(ref _CurrentDiaryView, value); }
+        }
+
+        public MyICommand<string> DiaryCommand { get; private set; }
+
+        private void DiaryViewChange(string destination)
+        {
+            switch (destination)
+            {
+                case "Home":
+                    CurrentDiaryView = diaryMainView;
+                    break;
+                case "Pencil":
+                    CurrentDiaryView = diaryWriteView;
+                    break;
+                case "Calendar":
+                    CurrentDiaryView = diaryCalendarView;
+                    break;
+                case "Board":
+                    CurrentDiaryView = diaryBoardView;
+                    break;
+                default:
+                    //CurrentView;
+                    break;
+            }
+        }
+    }
+}
